c6ede67e560f8c0e25889dcb60b7fe7409140785
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2024-04-10 23:27:06 -0300

    index, nuevo index, star rating, correciones en cards, iniciar sesions

--- main.js
+++ main.js
@@ -27,3 +27,32 @@ function cerrarModal() {
 document.querySelector(".boton").addEventListener("click", function() {
   window.location.href = "https://fashion-arte.vercel.app";
 });
+
+// sistema de vestidos
+
+document.addEventListener('DOMContentLoaded', function() {
+  const dressRatings = document.querySelectorAll('.dress-rating');
+
+  dressRatings.forEach((dressRating) => {
+    const dresses = dressRating.querySelectorAll('.dress');
+    
+    dresses.forEach((dress, index) => {
+      dress.addEventListener('click', function() {
+        const rating = index + 1;
+        setRating(dressRating, rating);
+      });
+    });
+  });
+
+  function setRating(dressRating, rating) {
+    const dresses = dressRating.querySelectorAll('.dress');
+    dresses.forEach((dress, index) => {
+      if (index < rating) {
+        dress.classList.add('filled');
+      } else {
+        dress.classList.remove('filled');
+      }
+    });
+    // Aquí puedes enviar el valor de rating al servidor o realizar otras acciones con él
+  }
+});
